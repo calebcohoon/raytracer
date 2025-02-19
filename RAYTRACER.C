ca50191c1f399308dce28b1efb41b153747926e7
Caleb Cohoon <caleb@poly3.dev>
2025-02-18 20:01:49 -0500

    Various improvements and simplifications

--- RAYTRACER.C
+++ RAYTRACER.C
@@ -1,405 +1,342 @@
+#include <conio.h>
 #include <dos.h>
 #include <i86.h>
-#include <conio.h>
 #include <math.h>
 
-#define INF                 32767   // Infinity for 16 bit compilers
-#define SW                  320      // Screen width
-#define SH                  200      // Screen height
-#define VPS                 1.0     // Viewport size
-#define PPZ                 1.0     // Viewport depth
-#define AMBIENT_LIGHT       0
-#define POINT_LIGHT         1
-#define DIRECTIONAL_LIGHT   2
+#define INF 32767 // Infinity for 16 bit compilers
+#define SW 320    // Screen width
+#define SH 200    // Screen height
+#define VPS 1.0   // Viewport size
+#define PPZ 1.0   // Viewport depth
+#define AMBIENT_LIGHT 0
+#define POINT_LIGHT 1
+#define DIRECTIONAL_LIGHT 2
+#define SCREEN_CENTER_X (SW / 2)
+#define SCREEN_CENTER_Y (SH / 2)
+#define VPS_SW (VPS / SW)
+#define VPS_SH (VPS / SH)
+#define ASPECT_RATIO 1.2f
 
-struct vector2 {
-    float x;
-    float y;
-};
+typedef struct {
+  float x;
+  float y;
+} vector2;
 
-struct vector3 {
-    float x;
-    float y;
-    float z;
-};
+typedef struct {
+  float x;
+  float y;
+  float z;
+} vector3;
 
-struct sphere {
-    float radius;
-    int specular;
-    char color;
-    struct vector3 center;
-};
+typedef struct {
+  float radius;
+  int specular;
+  char color;
+  vector3 center;
+} sphere;
 
-struct closest_sphere_ret {
-    struct sphere* sphere;
-    float closest_t;
-};
-
-struct light {
-    int type;
-    float intensity;
-    struct vector3 position;
-    struct vector3 direction;
-};
+typedef struct {
+  int type;
+  float intensity;
+  vector3 position;
+  vector3 direction;
+} light;
 
 // Build an optimized palette for the four main colors being used
 void init_palette() {
-    int color, shade;
-    unsigned char index;
-    float intensity;
-    unsigned char base_colors[4][3] = {
-        {0, 0, 63},   // Blue
-        {0, 63, 0},   // Green
-        {63, 0, 0},   // Red
-        {63, 63, 32}  // Yellow
-    };
+  int color, shade;
+  unsigned char index;
+  float intensity;
+  unsigned char base_colors[4][3] = {
+      {0, 0, 63},  // Blue
+      {0, 63, 0},  // Green
+      {63, 0, 0},  // Red
+      {63, 63, 32} // Yellow
+  };
 
-    for (color = 0; color < 4; color++) {
-        for (shade = 0; shade < 64; shade++) {
-            index = color * 64 + shade;
-            intensity = shade / 63.0f;
+  for (color = 0; color < 4; color++) {
+    for (shade = 0; shade < 64; shade++) {
+      index = color * 64 + shade;
+      intensity = shade / 63.0f;
 
-            outp(0x3C8, index);
+      outp(0x3C8, index);
 
-            // For the very last shade of yellow, just make it white
-            // so we have a color for the background
-            if (color == 3 && shade == 63) {
-                outp(0x3C9, (unsigned char)63);
-                outp(0x3C9, (unsigned char)63);
-                outp(0x3C9, (unsigned char)63);
-            }
-            else {
-                outp(0x3C9, (unsigned char)(base_colors[color][0] * intensity));
-                outp(0x3C9, (unsigned char)(base_colors[color][1] * intensity));
-                outp(0x3C9, (unsigned char)(base_colors[color][2] * intensity));
-            }
-        }
+      // For the very last shade of yellow, just make it white
+      // so we have a color for the background
+      if (color == 3 && shade == 63) {
+        outp(0x3C9, (unsigned char)63);
+        outp(0x3C9, (unsigned char)63);
+        outp(0x3C9, (unsigned char)63);
+      } else {
+        outp(0x3C9, (unsigned char)(base_colors[color][0] * intensity));
+        outp(0x3C9, (unsigned char)(base_colors[color][1] * intensity));
+        outp(0x3C9, (unsigned char)(base_colors[color][2] * intensity));
+      }
     }
+  }
 }
 
 void set_mode(unsigned char mode) {
-    union REGS regs;
-    regs.h.ah = 0;
-    regs.h.al = mode;
-    int86(0x10, &regs, &regs);
+  union REGS regs;
+
+  regs.h.ah = 0;
+  regs.h.al = mode;
+
+  int86(0x10, &regs, &regs);
 }
 
 void set_pixel(int x, int y, char color) {
-    char far* screen = MK_FP(0xA000, 0);
-    int ax, ay;
+  char far *screen = MK_FP(0xA000, 0);
+  int ax, ay;
 
-    ax = SW / 2 + x;
-    ay = SH / 2 - y;
+  ax = SCREEN_CENTER_X + x;
+  ay = SCREEN_CENTER_Y - y;
 
-    if (ax < 0 || ax >= SW || ay < 0 || ay >= SH) {
-        return;
-    }
+  if (ax < 0 || ax >= SW || ay < 0 || ay >= SH) {
+    return;
+  }
 
-    screen[ay * SW + ax] = color;
+  screen[ay * SW + ax] = color;
 }
 
 unsigned char shade_color(unsigned char color, float intensity) {
-    unsigned char shade;
-    if (intensity > 1.0f) intensity = 1.0f;
-    if (intensity < 0.0f) intensity = 0.0f;
+  unsigned char shade;
+  if (intensity > 1.0f)
+    intensity = 1.0f;
+  if (intensity < 0.0f)
+    intensity = 0.0f;
 
-    shade = (unsigned char)(intensity * 63.0f);
-    return color * 64 + shade;
+  shade = (unsigned char)(intensity * 63.0f);
+  return color * 64 + shade;
 }
 
-struct vector3 vec3_add(struct vector3* a, struct vector3* b) {
-    struct vector3 result;
+vector3 vec3_add(vector3 *a, vector3 *b) {
+  vector3 result;
 
-    result.x = a->x + b->x;
-    result.y = a->y + b->y;
-    result.z = a->z + b->z;
+  result.x = a->x + b->x;
+  result.y = a->y + b->y;
+  result.z = a->z + b->z;
 
-    return result;
+  return result;
 }
 
-struct vector3 vec3_sub(struct vector3* a, struct vector3* b) {
-    struct vector3 result;
+vector3 vec3_sub(vector3 *a, vector3 *b) {
+  vector3 result;
 
-    result.x = b->x - a->x;
-    result.y = b->y - a->y;
-    result.z = b->z - a->z;
+  result.x = a->x - b->x;
+  result.y = a->y - b->y;
+  result.z = a->z - b->z;
 
-    return result;
+  return result;
 }
 
-struct vector3 vec3_mul(struct vector3* v, float s) {
-    struct vector3 result;
+vector3 vec3_mul(vector3 *v, float s) {
+  vector3 result;
 
-    result.x = v->x * s;
-    result.y = v->y * s;
-    result.z = v->z * s;
+  result.x = v->x * s;
+  result.y = v->y * s;
+  result.z = v->z * s;
 
-    return result;
+  return result;
 }
 
-struct vector3 vec3_div(struct vector3* v, float d) {
-    struct vector3 result;
+vector3 vec3_div(vector3 *v, float d) {
+  vector3 result;
+  float recip = 1.0f / d;
 
-    result.x = v->x / d;
-    result.y = v->y / d;
-    result.z = v->z / d;
+  result.x = v->x * recip;
+  result.y = v->y * recip;
+  result.z = v->z * recip;
 
-    return result;
+  return result;
 }
 
-float vec3_len(struct vector3* v) {
-    return sqrt((v->x * v->x) + (v->y * v->y) + (v->z * v->z));
+float vec3_len(vector3 *v) {
+  return sqrt((v->x * v->x) + (v->y * v->y) + (v->z * v->z));
 }
 
-float vec3_dot(struct vector3* a, struct vector3* b) {
-    return (a->x * b->x) + (a->y * b->y) + (a->z * b->z);
+float vec3_dot(vector3 *a, vector3 *b) {
+  return (a->x * b->x) + (a->y * b->y) + (a->z * b->z);
 }
 
-struct vector2 intersect_ray_sphere(struct vector3* o,
-    struct vector3* d,
-    struct sphere* s) {
-    struct vector2 inf, t;
-    float r = s->radius;
-    struct vector3 co = vec3_sub(&s->center, o);
-    float a = vec3_dot(d, d);
-    float b = 2 * vec3_dot(&co, d);
-    float c = vec3_dot(&co, &co) - (r * r);
-    float discr = (b * b) - (4 * a * c);
+vector2 intersect_ray_sphere(vector3 *o, vector3 *d, sphere *s) {
+  vector2 inf, t;
+  float r = s->radius;
+  vector3 co = vec3_sub(o, &s->center);
+  float a = vec3_dot(d, d);
+  float b = 2 * vec3_dot(&co, d);
+  float c = vec3_dot(&co, &co) - (r * r);
+  float discr = (b * b) - (4 * a * c);
+  float sqrt_discr;
 
-    inf.x = INF;
-    inf.y = INF;
+  inf.x = INF;
+  inf.y = INF;
 
-    if (discr < 0) {
-        return inf;
+  if (discr < 0) {
+    return inf;
+  }
+
+  sqrt_discr = sqrt(discr);
+  t.x = (-b + sqrt_discr) / (2 * a);
+  t.y = (-b - sqrt_discr) / (2 * a);
+
+  return t;
+}
+
+sphere *closest_intersection(vector3 *o, vector3 *d, float t_min, float t_max,
+                             float *closest_t, int sphere_count,
+                             sphere *spheres) {
+  sphere *closest_sphere = 0;
+  int i = 0;
+  vector2 t;
+
+  *closest_t = INF;
+
+  for (i = 0; i < sphere_count; i++) {
+    t = intersect_ray_sphere(o, d, &spheres[i]);
+
+    if ((t.x > t_min && t.x < t_max) && t.x < *closest_t) {
+      *closest_t = t.x;
+      closest_sphere = &spheres[i];
     }
 
-    t.x = (-b + sqrt(discr)) / (2 * a);
-    t.y = (-b - sqrt(discr)) / (2 * a);
-
-    return t;
-}
-
-struct closest_sphere_ret closest_intersection(struct vector3* o,
-    struct vector3* d,
-    float t_min,
-    float t_max,
-    int sphere_count,
-    struct sphere* spheres) {
-    float closest_t = INF;
-    struct sphere* closest_sphere = 0;
-    struct closest_sphere_ret result;
-    int i = 0;
-    struct vector2 t;
-
-    for (i = 0; i < sphere_count; i++) {
-        t = intersect_ray_sphere(o, d, &spheres[i]);
-
-        if ((t.x > t_min && t.x < t_max) && t.x < closest_t) {
-            closest_t = t.x;
-            closest_sphere = &spheres[i];
-        }
-
-        if ((t.y > t_min && t.y < t_max) && t.y < closest_t) {
-            closest_t = t.y;
-            closest_sphere = &spheres[i];
-        }
+    if ((t.y > t_min && t.y < t_max) && t.y < *closest_t) {
+      *closest_t = t.y;
+      closest_sphere = &spheres[i];
     }
+  }
 
-    result.sphere = closest_sphere;
-    result.closest_t = closest_t;
-
-    return result;
+  return closest_sphere;
 }
 
-struct vector3 reflect_ray(struct vector3* r, struct vector3* n) {
-    struct vector3 R;
+vector3 reflect_ray(vector3 *r, vector3 *n) {
+  vector3 R;
 
-    R = vec3_mul(n, 2);
-    R = vec3_mul(&R, vec3_dot(n, r));
-    R = vec3_sub(r, &R);
+  R = vec3_mul(n, 2);
+  R = vec3_mul(&R, vec3_dot(n, r));
+  R = vec3_sub(&R, r);
 
-    return R;
+  return R;
 }
 
-float compute_lighting(struct vector3* p,
-    struct vector3* n,
-    struct vector3* v,
-    int s,
-    int light_count,
-    struct light* lights,
-    int sphere_count,
-    struct sphere* spheres) {
-    float i = 0.0f;
-    int li;
-    struct vector3 L, R;
-    float n_dot_l, r_dot_v, t_max;
-    struct closest_sphere_ret result;
+float compute_lighting(vector3 *p, vector3 *n, vector3 *v, int s,
+                       int light_count, light *lights, int sphere_count,
+                       sphere *spheres) {
+  int li;
+  float i = 0.0f;
+  float n_dot_l, r_dot_v, t_max, closest_t;
+  float n_len = vec3_len(n);
+  float v_len = vec3_len(v);
+  vector3 L, R;
+  sphere *hit_sphere;
 
-    for (li = 0; li < light_count; li++) {
-        if (lights[li].type == AMBIENT_LIGHT) {
-            i += lights[li].intensity;
+  for (li = 0; li < light_count; li++) {
+    if (lights[li].type == AMBIENT_LIGHT) {
+      i += lights[li].intensity;
+    } else {
+      if (lights[li].type == POINT_LIGHT) {
+        L = vec3_sub(&lights[li].position, p);
+        t_max = 1.0f;
+      } else {
+        L = lights[li].direction;
+        t_max = INF;
+      }
+
+      hit_sphere = closest_intersection(p, &L, 0.001f, t_max, &closest_t,
+                                        sphere_count, spheres);
+
+      // Cast shadow
+      if (hit_sphere != 0) {
+        continue;
+      }
+
+      // Diffuse
+      n_dot_l = vec3_dot(n, &L);
+      if (n_dot_l > 0) {
+        i += lights[li].intensity * n_dot_l / (n_len * vec3_len(&L));
+      }
+
+      // Specular
+      if (s != -1) {
+        R = reflect_ray(&L, n);
+        r_dot_v = vec3_dot(&R, v);
+        if (r_dot_v > 0) {
+          i += lights[li].intensity * pow(r_dot_v / (vec3_len(&R) * v_len), s);
         }
-        else
-        {
-            if (lights[li].type == POINT_LIGHT) {
-                L = vec3_sub(p, &lights[li].position);
-                t_max = 1.0f;
-            }
-            else
-            {
-                L = lights[li].direction;
-                t_max = INF;
-            }
-
-            result = closest_intersection(p, &L, 0.001f,
-                t_max, sphere_count, spheres);
-
-            // Cast shadow
-            if (result.sphere != 0) {
-                continue;
-            }
-
-            // Diffuse
-            n_dot_l = vec3_dot(n, &L);
-            if (n_dot_l > 0) {
-                i += lights[li].intensity * n_dot_l / (vec3_len(n)
-                    * vec3_len(&L));
-            }
-
-            // Specular
-            if (s != -1) {
-                R = reflect_ray(&L, n);
-                r_dot_v = vec3_dot(&R, v);
-                if (r_dot_v > 0) {
-                    i += lights[li].intensity
-                        * pow(r_dot_v / (vec3_len(&R) * vec3_len(v)), s);
-                }
-            }
-        }
+      }
     }
+  }
 
-    return i;
+  return i;
 }
 
-unsigned char trace_ray(struct vector3* o,
-    struct vector3* d,
-    float t_min,
-    float t_max,
-    int sphere_count,
-    struct sphere* spheres,
-    int light_count,
-    struct light* lights) {
-    struct closest_sphere_ret result;
-    struct vector3 p, n, inter, d_neg;
-    float light_inten = 0;
+unsigned char trace_ray(vector3 *o, vector3 *d, float t_min, float t_max,
+                        int sphere_count, sphere *spheres, int light_count,
+                        light *lights) {
+  sphere *hit_sphere;
+  vector3 p, n, inter, d_neg;
+  float light_inten = 0, closest_t;
 
-    result = closest_intersection(o, d, t_min, t_max,
-        sphere_count, spheres);
+  hit_sphere = closest_intersection(o, d, t_min, t_max, &closest_t,
+                                    sphere_count, spheres);
 
-    if (result.sphere == 0) {
-        // Full white
-        return shade_color(3, 1);
-    }
+  if (hit_sphere == 0) {
+    // Full white
+    return shade_color(3, 1);
+  }
 
-    inter = vec3_mul(d, result.closest_t);
-    p = vec3_add(o, &inter);
-    n = vec3_sub(&result.sphere->center, &p);
-    n = vec3_div(&n, vec3_len(&n));
-    d_neg = vec3_mul(d, -1);
+  inter = vec3_mul(d, closest_t);
+  p = vec3_add(o, &inter);
+  n = vec3_sub(&p, &hit_sphere->center);
+  n = vec3_div(&n, vec3_len(&n));
+  d_neg = vec3_mul(d, -1);
 
-    light_inten = compute_lighting(&p, &n, &d_neg, result.sphere->specular,
-        light_count, lights, sphere_count, spheres);
+  light_inten = compute_lighting(&p, &n, &d_neg, hit_sphere->specular,
+                                 light_count, lights, sphere_count, spheres);
 
-    return shade_color(result.sphere->color, light_inten);
+  return shade_color(hit_sphere->color, light_inten);
 }
 
-struct vector3 canvas_to_viewport(int x, int y) {
-    struct vector3 pos;
+vector3 canvas_to_viewport(int x, int y) {
+  vector3 pos;
 
-    pos.x = x * VPS / SW;
-    pos.y = y * VPS / SH;
-    pos.z = PPZ;
+  pos.x = x * VPS_SW * ASPECT_RATIO;
+  pos.y = y * VPS_SH;
+  pos.z = PPZ;
 
-    return pos;
+  return pos;
 }
 
 int main(void) {
-    struct vector3 o;
-    struct sphere spheres[4];
-    struct light lights[3];
-    int x, y;
+  vector3 o = {0, 0, 0};
+  sphere spheres[] = {
+      {1.0f, 500, 2, {0, -1, 3}},       /* Red sphere */
+      {1.0f, 10, 1, {-2, 0, 4}},        /* Green sphere */
+      {1.0f, 500, 0, {2, 0, 4}},        /* Blue sphere */
+      {5000.0f, 1000, 3, {0, -5001, 0}} /* Yellow ground */
+  };
+  light lights[] = {{AMBIENT_LIGHT, 0.2f, {0, 0, 0}, {0, 0, 0}},
+                    {POINT_LIGHT, 0.6f, {2, 1, 0}, {0, 0, 0}},
+                    {DIRECTIONAL_LIGHT, 0.2f, {0, 0, 0}, {1, 4, 4}}};
+  int x, y;
 
-    // Set camera position
-    o.x = 0;
-    o.y = 0;
-    o.z = 0;
+  set_mode(0x13);
 
-    // Configure colored spheres
-    spheres[0].radius = 1;
-    spheres[0].center.x = 0;
-    spheres[0].center.y = -1;
-    spheres[0].center.z = 3;
-    spheres[0].specular = 500;  // Shiny
-    spheres[0].color = 2;       // Red
+  // Important that this runs AFTER setting the VGA subsystem
+  // into graphics mode 13H
+  init_palette();
 
-    spheres[1].radius = 1;
-    spheres[1].center.x = -2;
-    spheres[1].center.y = 0;
-    spheres[1].center.z = 4;
-    spheres[1].specular = 10;   // Somewhat shiny
-    spheres[1].color = 1;       // Green
-
-    spheres[2].radius = 1;
-    spheres[2].center.x = 2;
-    spheres[2].center.y = 0;
-    spheres[2].center.z = 4;
-    spheres[2].specular = 500;  // Shiny
-    spheres[2].color = 0;       // Blue
-
-    spheres[3].radius = 5000;
-    spheres[3].center.x = 0;
-    spheres[3].center.y = -5001;
-    spheres[3].center.z = 0;
-    spheres[3].specular = 1000; // Very shiny
-    spheres[3].color = 3;       // Yellow
-
-    // Configure lights
-    lights[0].type = AMBIENT_LIGHT;
-    lights[0].intensity = 0.2f;
-
-    lights[1].type = POINT_LIGHT;
-    lights[1].intensity = 0.6f;
-    lights[1].position.x = 2;
-    lights[1].position.y = 1;
-    lights[1].position.z = 0;
-
-    lights[2].type = DIRECTIONAL_LIGHT;
-    lights[2].intensity = 0.2f;
-    lights[2].direction.x = 1;
-    lights[2].direction.y = 4;
-    lights[2].direction.z = 4;
-
-    set_mode(0x13);
-
-    // Important that this runs AFTER setting the VGA subsystem
-    // into graphics mode 13H
-    init_palette();
-
-    for (y = SH / 2; y >= -SH / 2; y--) {
-        for (x = -SW / 2; x <= SW / 2; x++) {
-            struct vector3 d;
-            unsigned char color;
-
-            d = canvas_to_viewport(x, y);
-            color = trace_ray(&o, &d, 1,
-                INF, 4, spheres, 3, lights);
-            set_pixel(x, y, color);
-        }
+  for (y = SH / 2; y >= -SH / 2; y--) {
+    for (x = -SW / 2; x <= SW / 2; x++) {
+      vector3 d = canvas_to_viewport(x, y);
+      unsigned char color = trace_ray(&o, &d, 1, INF, 4, spheres, 3, lights);
+      set_pixel(x, y, color);
     }
+  }
 
-    getch();
+  getch();
 
-    set_mode(0x03);
+  set_mode(0x03);
 
-    return 0;
+  return 0;
 }
